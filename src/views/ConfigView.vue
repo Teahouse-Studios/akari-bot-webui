3897efa04ae82369fe07ce05ee9b55d5b7c1b1c0
多羅狼 <dorowolf314@gmail.com>
2024-12-30 17:40:58 +0800

    u

--- src/views/ConfigView.vue
+++ src/views/ConfigView.vue
@@ -3,9 +3,9 @@
     <el-tabs v-model="activeTab" @tab-click="handleTabClick">
       <el-tab-pane v-for="file in configFiles" :key="file" :label="file" :name="file"></el-tab-pane>
     </el-tabs>
-
-    <div class="editor-header">
       <!--
+    <div class="editor-header">
+
       <el-button-group>
         <el-button
           :type="editorMode === 'tab' ? 'primary' : 'default'"
@@ -20,7 +20,17 @@
           <i class="mdi mdi-code-block-brackets"></i>
         </el-button>
       </el-button-group>
+
+    </div>
       -->
+    <div class="editor-body">
+      <codemirror
+        ref="editor"
+        v-model="editorContent"
+        :options="editorOptions"
+      ></codemirror>
+    </div>
+<div class="editor-footer">
       <div class="editor-actions">
         <el-button type="warning" @click="resetConfig">
           <i class="mdi mdi-restart"></i> 重置
@@ -29,18 +39,15 @@
           <i class="mdi mdi-content-save-outline"></i> 应用
         </el-button>
       </div>
-    </div>
-
-    <div class="editor-body">
-      <div ref="editor" class="editor"></div>
-    </div>
+</div>
   </div>
 </template>
 
 <script>
 import axios from '@/axios';
-import { onMounted, ref, watch } from 'vue';
-import { EditorView, basicSetup } from 'codemirror';
+import { javascript } from '@codemirror/lang-javascript';
+import { EditorView } from '@codemirror/view';
+import { basicSetup } from 'codemirror';
 import { EditorState } from '@codemirror/state';
 import { oneDark } from '@codemirror/theme-one-dark';
 
@@ -48,11 +55,16 @@ export default {
   name: 'ConfigView',
   data() {
     return {
-      activeTab: 'pipeline.json',
-      configFiles: ['command.json', 'pipeline.json', 'platform.json', 'provider.json', 'system.json'],
-      configSections: [],
+      activeTab: 'pipeline.toml',
+      configFiles: ['command.toml', 'pipeline.toml', 'platform.toml', 'provider.toml', 'system.toml'],
       editorContent: '',
-      editorInstance: null,
+      editorView: null,
+      editorOptions: {
+        lineWrapping: true, // 自动换行
+        theme: oneDark, // 使用 oneDark 主题
+        lineNumbers: true, // 显示行号
+        scrollbarStyle: 'native', // 使用原生滚动条
+      }
     };
   },
   methods: {
@@ -62,11 +74,10 @@ export default {
     fetchConfig(fileName) {
       axios.get(`/api/config/${fileName}`)
         .then(response => {
-          const data = response.data;
-          this.editorContent = data;
+          this.editorContent = response.data;
           this.updateEditorContent();
         })
-        .catch(error => {
+        .catch(() => {
           this.$message.error('请求失败，请稍后再试');
         });
     },
@@ -74,79 +85,62 @@ export default {
       this.fetchConfig(this.activeTab);
     },
     applyConfig() {
-      const payload = {
-        raw: this.editorContent, // 获取编辑器中的内容
-      };
-
-
-      axios.post(`/api/config/${this.activeTab}`, payload)
+      axios.post(`/api/config/${this.activeTab}`, { raw: this.editorContent })
         .then(() => {
           this.$message.success('配置已成功更新！');
         })
-        .catch(error => {
+        .catch(() => {
           this.$message.error('应用配置失败，请稍后再试');
         });
     },
-    buildPayload() {},
+    updateEditorContent() {
+      if (this.editorView) {
+        this.editorView.dispatch({ changes: { from: 0, to: this.editorView.state.doc.length, insert: this.editorContent } });
+      }
+    }
   },
   mounted() {
     this.fetchConfig(this.activeTab);
-  },
+    const state = EditorState.create({
+      doc: this.editorContent,
+      extensions: [javascript(), basicSetup, oneDark]
+    });
+    this.editorView = new EditorView({
+      state,
+      parent: this.$refs.editor
+    });
+  }
 };
 </script>
 
 <style scoped>
-
-.el-tabs ::v-deep .el-tabs__item {
+.el-tabs ::v-deep(.el-tabs__item) {
   color: #333;
 }
 
-body.dark-mode .el-tabs ::v-deep .el-tabs__item {
+body.dark-mode .el-tabs ::v-deep(.el-tabs__item) {
   color: white;
 }
 
-.el-tabs ::v-deep .el-tabs__item.is-active {
+.el-tabs ::v-deep(.el-tabs__item.is-active) {
   color: #0091ff;
 }
 
-body.dark-mode .el-tabs ::v-deep .el-tabs__item.is-active {
+body.dark-mode .el-tabs ::v-deep(.el-tabs__item.is-active) {
   color: #0091ff;
 }
 
-.el-tabs ::v-deep .el-tabs__active-bar {
+.el-tabs ::v-deep(.el-tabs__active-bar) {
   background-color: #0091ff;
 }
 
-
-.el-tabs {
-  padding-left: 16px;
-  padding-right: 16px;
-  padding-top: 10px;
-}
-
 .editor-container {
-  border-radius: 4px;
   display: flex;
   flex-direction: column;
-  height: 560px;  /* 让容器充满窗口 */
 }
 
 body.dark-mode .editor-container {
   color: white;
-}
-
-.editor-header {
-  display: flex;
-  justify-content: flex-end;
-  padding: 16px;
-  padding-top: 0;
-  gap: 20px;
-  flex-wrap: nowrap;
-}
-
-.el-button-group {
-  display: flex;
-  white-space: nowrap;
 }
 
 .editor-actions {
@@ -157,14 +151,30 @@ body.dark-mode .editor-container {
 }
 
 .editor-body {
-  padding-left: 16px;
-  padding-right: 16px;
-  flex-grow: 1;  /* 让编辑器主体部分填充剩余空间 */
-  overflow: hidden;  /* 内容溢出时可以滚动 */
+  flex-grow: 1;
+  height: 50vh;
+  overflow: auto;
 }
 
-.editor {
-  height: 100%;
+::v-deep(.cm-editor) {
+  height: 50vh;
+}
+
+.codemirror-container {
+  height: 50vh;
+  position: relative; /* 为了确保 CodeMirror 编辑器可以填充容器 */
+}
+
+.editor-footer {
+  display: flex;
+  justify-content: flex-start;
+  padding-top: 16px;
+  flex-wrap: nowrap;
+}
+
+.el-button-group {
+  display: flex;
+  white-space: nowrap;
 }
 
 .el-button {
