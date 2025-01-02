b9bf01b6bdaef1828d8efa6e8ee59779f88885ec
多羅狼 <Dorowolf314@gmail.com>
2025-01-03 01:53:23 +0800

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,33 +1,49 @@
 import axios from 'axios'
-import Cookies from 'js-cookie' // 引入 js-cookie
+import Cookies from 'js-cookie'
 
-axios.defaults.baseURL = process.env.VUE_APP_API_URL
-axios.defaults.timeout = 50000
-
-axios.interceptors.request.use(config => {
-  const csrfToken = Cookies.get('csrfToken')
-  if (csrfToken) {
-    config.headers['X-CSRF-TOKEN'] = csrfToken
+const loadConfig = async () => {
+  try {
+    const response = await fetch('/config.json')
+    const config = await response.json()
+    return config
+  } catch () {
+    return {}
   }
+}
 
-  const deviceToken = Cookies.get('deviceToken')
-  if (deviceToken && !config.url.includes('/auth')) {
-    config.headers.Authorization = `Bearer ${deviceToken}`
-  }
+const initAxios = async () => {
+  const config = await loadConfig()
 
-  return config
-}, error => {
-  return Promise.reject(error)
-})
+  axios.defaults.baseURL = config.API_URL || process.env.VUE_APP_API_URL  // 如果 config.json 没有提供 API_URL, 使用环境变量
+  axios.defaults.timeout = 50000
 
-// 响应拦截器
-axios.interceptors.response.use(
-  response => {
-    return response
-  },
-  error => {
-    return Promise.reject(error)
-  }
-)
+  axios.interceptors.request.use(
+    (config) => {
+      const csrfToken = Cookies.get('csrfToken')
+      if (csrfToken) {
+        config.headers['X-CSRF-TOKEN'] = csrfToken
+      }
+      const deviceToken = Cookies.get('deviceToken')
+      if (deviceToken && !config.url.includes('/auth')) {
+        config.headers.Authorization = `Bearer ${deviceToken}`
+      }
+      return config
+    },
+    (error) => {
+      return Promise.reject(error)
+    }
+  )
+
+  axios.interceptors.response.use(
+    (response) => {
+      return response
+    },
+    (error) => {
+      return Promise.reject(error)
+    }
+  )
+}
+
+initAxios()
 
 export default axios
