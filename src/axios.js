f20556cf51e791577a3b86d42fc3e50ecbd6f05f
多羅狼 <dorowolf314@gmail.com>
2025-01-03 17:39:09 +0800

    u

--- src/axios.js
+++ src/axios.js
@@ -1,30 +1,42 @@
 import axios from "axios";
 
 const initAxios = async () => {
-  axios.defaults.baseURL = process.env.VUE_APP_API_URL; // 如果 config.json 没有提供 API_URL, 使用环境变量
-  axios.defaults.timeout = 50000;
+  // 首先获取配置文件
+  const response = await fetch('/config.json');
+  const config = await response.json();
 
+  // 获取api_url
+  const apiUrl = config.api_url;
+
+  // 配置axios
+  axios.defaults.baseURL = apiUrl;
+  console.log('Axios baseURL:', axios.defaults.baseURL);
+  axios.defaults.timeout = 50000;
   axios.defaults.withCredentials = true;
 
+  // 添加请求拦截器
   axios.interceptors.request.use(
     (config) => {
       return config;
     },
     (error) => {
       return Promise.reject(error);
-    },
+    }
   );
 
+  // 添加响应拦截器
   axios.interceptors.response.use(
     (response) => {
       return response;
     },
     (error) => {
       return Promise.reject(error);
-    },
+    }
   );
+
+  return axios;
 };
 
-initAxios();
-
-export default axios;
+// 调用初始化函数并导出axios
+const axiosInstance = await initAxios();
+export default axiosInstance;
