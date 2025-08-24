604581c6eb6d6ae080024567dd08ee6975b53985
多羅狼 <Dorowolf314@gmail.com>
2025-08-24 12:34:29 +0800

    Add not found page

---
+++ src/pages/notFound.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-result 
+    icon="error"
+    :title="$t('not_found.result.title')"
+    :sub-title="$t('not_found.result.subtitle')"
+  >
+    <template #extra>
+      <el-button type="primary" @click="goHome">{{ $t('button.back') }}</el-button>
+    </template>
+  </el-result>
+</template>
+
+<script>
+import { useI18n } from 'vue-i18n'
+import { useRouter } from 'vue-router'
+
+export default {
+  name: 'EmptyView',
+  data() {
+    const router = useRouter()
+    const { t } = useI18n()
+
+    return {
+      router,
+      t,
+    }
+  },
+  methods: {
+    goHome() {
+      this.router.push('/')
+    },
+  },
+}
+</script>
--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { routes } from 'vue-router/auto-routes'
+import NotFoundPage from '@/pages/notFound.vue'
 
 const router = createRouter({
   history: createWebHistory("/webui/"),
@@ -8,7 +9,12 @@ const router = createRouter({
       path: '/',
       redirect: '/dashboard'
     },
-    ...routes
+    ...routes,
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: NotFoundPage
+    }
   ],
 })
 
